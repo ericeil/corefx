ae59ccf155c26363c74895706f33350aa943c755
Eric Eilebrecht <ericeil@microsoft.com>
2016-09-22 16:36:53 -0700

    more

--- src/Common/src/System/Runtime/Serialization/OptionalFieldAttribute.cs
+++ src/Common/src/System/Runtime/Serialization/OptionalFieldAttribute.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System
+namespace System.Runtime.Serialization
 {
     [AttributeUsageAttribute(AttributeTargets.Field, Inherited = false)]
     public sealed partial class OptionalFieldAttribute : Attribute
